d3da3641a0f6e2b7073ad1a68bb0dd3d74fd92dc
Abhishek Gautam <abhishekgautam388@gmail.com>
2021-09-05 20:12:33 +0530

    UI added

--- Assgn7-WeatherApp/app.js
+++ Assgn7-WeatherApp/app.js
@@ -4,6 +4,9 @@ const temp = document.querySelector('.temp');
 const t_minmax = document.querySelector('.t_minmax');
 const loc = document.querySelector('.loc');
 const type = document.querySelector('.type');
+const humanDate = document.querySelector('.date');
+const humid = document.querySelector('.humid');
+const pressure = document.querySelector('.pressure');
 
 function getWeather(city){
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
@@ -16,18 +19,31 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}
         const mintemp = data.main.temp_min;
         const maxtemp = data.main.temp_max;
         const place = data.name;
+        const country = data.sys.country;
         const w_type = data.weather[0].main;
-
-        loc.textContent = `${place}`;
+        const w_pres = data.main.pressure;
+        const w_humid = data.main.humidity;
+       
+        const unixDate = data.dt;
+        const ms = unixDate*1000;
+        const dateObject = new Date(ms);
+        const day = dateObject.toLocaleString("en-US", {weekday:"long"});
+        const month = dateObject.toLocaleString("en-US", {month:"long"});
+        const date = dateObject.toLocaleString("en-US", {day:"numeric"});
+        const hr = dateObject.toLocaleString("en-US", {hour:"numeric", minute: "numeric", hour12: true});
+        
+        humanDate.textContent = `${day}, ${date} ${month} ${hr}`;
+        loc.textContent = `${place}, ${country}`;
         temp.textContent = `${tempData.toFixed(1)} °C`;
-        t_minmax.textContent = `Hi: ${mintemp.toFixed(0)} °C | Lo: ${maxtemp.toFixed(0)} °C`;
+        t_minmax.textContent = `Hi: ${mintemp.toFixed(1)}°C | Lo: ${maxtemp.toFixed(1)}°C`;
         type.textContent = `${w_type}`;
-
+        humid.textContent = `Humidity: ${w_humid}%`;
+        pressure.textContent = `Pressure: ${w_pres} hPa`;
     
     })
     
     .catch((err)=>{
-        console.log(err.message);
+        alert("Invalid city name. Please try again!");
     });
 }
 
