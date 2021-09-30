5232d366e80394176b3664b9effcea80522e5e62
Abhishek Gautam <abhishekgautam388@gmail.com>
2021-09-30 09:15:05 +0530

    Twilio integrated

--- Assgn8-VisitorApp/send_sms.js
+++ Assgn8-VisitorApp/send_sms.js
@@ -1,6 +1,7 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
+
 module.exports = (phone)=>{
 client.messages
   .create({
